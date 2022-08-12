9fd00071947bfddeac34c8e34247fb423b41e85f
bioresestance <aaron.bromma@gmail.com>
2022-08-12 21:14:56 +0000

    reorganizing and added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello World!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
