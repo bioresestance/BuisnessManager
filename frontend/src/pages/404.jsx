7e545037c9c0113b86846f6c6331c9d126be4273
bioresestance <aaron.bromma@gmail.com>
2022-08-17 23:08:17 +0000

    commiting the 404

---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,12 @@
+export default function Error404() {
+  return (
+    <div>
+      <h1> Erorr 404</h1>
+      <p>
+        {" "}
+        It seems the link you followed is either dead or miss-typed. Please
+        check the URL and try again!
+      </p>
+    </div>
+  );
+}
