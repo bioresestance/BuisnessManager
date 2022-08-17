3713a891210905abfdd8c9d211f7792d0f3f5c3c
Aaron Bromma <aaron.bromma@gmail.com>
2022-08-16 22:24:40 -0700

    All sorts of web updates

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
 
