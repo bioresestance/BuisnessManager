fba8fa3e7ac11a2e8468aad2583b3e5991dc4ea5
bioresestance <aaron.bromma@gmail.com>
2022-08-17 23:08:34 +0000

    various changes

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,14 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Header(props) {
   return (
     <nav className="flex h-12 bg-slate-200 ">
-      <div className="flex grow">
+      <div className="flex grow ">
         <button
           type="button"
           id="sidebarCollapse"
-          className=""
+          className="rounded bg-slate-100  place-self-center p-2 m-2"
           onClick={props.updateSidebarState}
         >
-          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon
+            icon={faBars}
+            className="rounded bg-slate-100  place-self-center w-full"
+          />
         </button>
       </div>
       <div className="flex align-middle p-0 m-0">
