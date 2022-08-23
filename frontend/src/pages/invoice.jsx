751115eaf1c61ac48d305f205cb165407403c534
bioresestance <aaron.bromma@gmail.com>
2022-08-23 21:03:12 +0000

    nested routes

--- frontend/src/pages/invoice.jsx
+++ frontend/src/pages/invoice.jsx
@@ -3,26 +3,11 @@ import { Link, Outlet, Route, Routes } from "react-router-dom";
 
 export default function Invoice() {
   return (
-    <>
-      <Routes location="/invoices">
-        <Route path="/" element={<InvoiceFrame />}></Route>;
-      </Routes>
-
-      <div class="tabs">
-        <Link to="/invoices/">Tab 1</Link>
-        <a class="tab tab-active">Tab 2</a>
-        <a class="tab">Tab 3</a>
-      </div>
-
+    <div>
       <h1>hello</h1>
-      <Outlet />
-    </>
+      <Routes>
+        <Route path="new_client" element={<ClientForm />}></Route>;
+      </Routes>
+    </div>
   );
-}
-
-function InvoiceFrame() {
-  <>
-    <h1> Invoices</h1>
-    <ClientForm />
-  </>;
 }
