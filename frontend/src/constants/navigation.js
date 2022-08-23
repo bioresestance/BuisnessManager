cfeb0c443390f75fdaa1da02b78d0e53ef690d15
bioresestance <aaron.bromma@gmail.com>
2022-08-23 21:02:58 +0000

    Added navigation tabs to header

--- frontend/src/constants/navigation.js
+++ frontend/src/constants/navigation.js
@@ -1,18 +1,34 @@
-
 export const navigation = [
+  {
+    icon: "",
+    text: "Home",
+    link: "/",
+  },
+  {
+    icon: "",
+    text: "Settings",
+    link: "/settings",
+  },
+  {
+    icon: "",
+    text: "Invoices",
+    link: "/invoices",
+  },
+];
+
+export const sub_navigation = {
+  invoices: [
     {
-        "icon": "",
-        "text": "Home",
-        "link": "/"
-    }, 
-    {
-        "icon": "",
-        "text": "Settings",
-        "link": "/settings"
+      tab_name: "New Invoice",
+      tab_link: "",
     },
     {
-        "icon": "",
-        "text": "Invoices",
-        "link": "/invoices"
-    }
-];
+      tab_name: "New Client",
+      tab_link: "new_client",
+    },
+    {
+      tab_name: "Clients",
+      tab_link: "clients",
+    },
+  ],
+};
