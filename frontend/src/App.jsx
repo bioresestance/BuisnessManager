c9addab445f1bbadbceea7fb3df5bf2d8dd7f3ae
bioresestance <aaron.bromma@gmail.com>
2022-08-24 21:32:08 +0000

    work on messages, not fully working yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
 import SideBar from "./components/sidebar";
 import Header from "./components/header";
-import Home from "./pages/home";
-import { useState } from "react";
-import { Outlet, Link } from "react-router-dom";
+import Message from "./components/mesage";
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   // Keep track of the state of the sidebar, wether its displayed or not.
   const [sidebarState, setSideBarState] = useState(true);
+  const [messages, setMessages] = useState([
+    // { message: "Hello world", type: "success" },
+    // { message: "Hello world", type: "warning" },
+  ]);
+
+  const deleteMessage = (id) => {
+    console.log(id);
+    setMessages((prevVal) => {
+      prevVal.splice(id, 1);
+      return prevVal;
+    });
+    console.log(messages);
+  };
+
+  let messageComponents = messages.map((message, index) => {
+    return (
+      <Message
+        key={index}
+        id={index}
+        message={message.message}
+        type={message.type}
+        close={deleteMessage}
+        timeout={message.timeout}
+      />
+    );
+  });
 
   // Main application of the site. Implements a 2 column site, with a sidebar and main body content.
   return (
@@ -19,10 +45,11 @@ export default function App() {
       )}
 
       {/* Main body content */}
-      <main className="w-full">
+      <main className="w-full flex flex-col">
         <Header
           updateSidebarState={() => setSideBarState((prevVal) => !prevVal)}
         />
+        {messageComponents}
         {/* Outputs the content of the route */}
         <Outlet />
       </main>
--- frontend/src/pages/invoice.jsx
+++ frontend/src/pages/invoice.jsx
@@ -4,12 +4,10 @@ import { Link, Outlet, Route, Routes } from "react-router-dom";
 
 export default function Invoice() {
   return (
-    <div>
-      <h1>hello</h1>
-      <Routes>
-        <Route path="new_client" element={<NewClientForm />}></Route>;
-        <Route path="clients" element={<ClientForm />}></Route>;
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="" element={<NewClientForm />}></Route>;
+      <Route path="new_client" element={<NewClientForm />}></Route>;
+      <Route path="clients" element={<ClientForm />}></Route>;
+    </Routes>
   );
 }
