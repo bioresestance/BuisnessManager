ed0cd0f69b50ec9bedc15a12fa64a3ef84d5fea4
bioresestance <aaron.bromma@gmail.com>
2022-10-19 17:47:25 +0000

    fix for urls

--- backend/server/routes/settings_route.py
+++ backend/server/routes/settings_route.py
@@ -16,7 +16,7 @@ class SettingsRoute(Resource):
         print(api.payload)
 
 
-@api.route("/<int:group_id>")
+@api.route("/<int:group_id>/")
 @api.param("group_id", "Group id to work with")
 @api.doc(params={"group_id": "Group id to work with"})
 class SettingsGroupRoute(Resource):
@@ -30,6 +30,8 @@ class SettingsGroupRoute(Resource):
             return Response("Erorr 404: Group ID not in Settings", 404)
 
         list_items = list(serverconfig.config_data)
+
+        print(f"Received new setting update for {group_id}")
 
         # Creates new object based on currect index, sets data from payload.
         new_setting = serverconfig.config_data[list_items[group_id]].__class__(
